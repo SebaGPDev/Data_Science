7de1ffb29c3352dad4853c5bc573cc7517526b1e
SebaGPDev <sebakpo120@gmail.com>
2024-01-02 17:27:56 -0300

    Added section on Python syntax with an example of working with strings

---
+++ python_basico.py
@@ -0,0 +1,15 @@
+"""
+Módulo de Utilidades Básicas en Python.
+
+Este módulo proporciona funciones y utilidades básicas para ayudar en tareas comunes de Python.
+Incluye ejemplos y conceptos fundamentales.
+"""
+
+# ! 2. Sintaxis en Python
+# Guardar una var un string e imprimir el caracter en la posición 0, longitud y el string completo
+
+ANY_LETTER = "Hola mundo"
+
+print(ANY_LETTER[0])
+print(len(ANY_LETTER))
+print(ANY_LETTER)
